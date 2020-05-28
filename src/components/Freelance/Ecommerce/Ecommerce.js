c7af68ef464d764ae9b59fe22ad26f1779a3123b
DavidKe91 <davidgkennedy@hotmail.com>
2020-05-28 18:46:32 +0100

    GH Pages

--- src/components/Freelance/Ecommerce/Ecommerce.js
+++ src/components/Freelance/Ecommerce/Ecommerce.js
@@ -26,31 +26,6 @@ class Ecommerce extends Component {
                     <div className="col-6">
                         <img className="img-fluid" src="https://s3-eu-west-1.amazonaws.com/davidgkennedy.com/Portfolio+Images/E-Commerce+Inline.png" alt="Bespoke Design" />
                     </div>
-                    <div className="aboutMeCols">
-                        <div className="row mb-5">
-                            <div className="col-12 col-md-4">
-                                <div className="categories">
-                                    {/* <FontAwesomeIcon icon={faCode} /> */}
-                                    <h4>Development</h4>
-                                    <p>I have over 6 years of experience working in web development. The primary language that I work with is Javascript. I use this Javascript experience to work with frameworks like React and Vue.</p>
-                                </div>
-                            </div>
-                            <div className="col-12 col-md-4">
-                                <div className="categories">
-                                    {/* <FontAwesomeIcon icon={faTerminal} /> */}
-                                    <h4>Dev Tools</h4>
-                                    <p>Throughout my career I have worked with many development processes and tools that greatly simplify and shorten the development lifecycle. Tools such as Jenkins, Git and Docker help with the continuous delivery of high quality code.</p>
-                                </div>
-                            </div>
-                            <div className="col-12 col-md-4">
-                                <div className="categories">
-                                    {/* <FontAwesomeIcon icon={faPaintBrush} /> */}
-                                    <h4>UX Design</h4>
-                                    <p>My previous experience as a graphic designer enables me to work with designers to create seamless user interfaces. I am proficient in tools such as Adobe's Creative Suite and Sketch which makes converting mockups to code effortless.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div className="col-12">
                         <Link to="/about"><button className="aboutBtn">Learn More About Me <FontAwesomeIcon icon={faArrowRight} /></button></Link>
                     </div>
