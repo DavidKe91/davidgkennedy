5005e466ec2ac00abc6b32c6f72b6cbf2543a7c5
DavidKe91 <davidgkennedy@hotmail.com>
2020-05-28 18:45:11 +0100

    Updates

---
+++ precache-manifest.22673168913e2c7acb7ac4722ab3e9fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6282d518e89b58b39b7b4a7d5f2afdbe",
+    "url": "/davidgkennedy.com/index.html"
+  },
+  {
+    "revision": "cdd830e24a47de1a9cd1",
+    "url": "/davidgkennedy.com/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "faa8620fedf7fa06ce7d",
+    "url": "/davidgkennedy.com/static/css/main.f2ae8150.chunk.css"
+  },
+  {
+    "revision": "cdd830e24a47de1a9cd1",
+    "url": "/davidgkennedy.com/static/js/2.37820ea3.chunk.js"
+  },
+  {
+    "revision": "46b0fb03a333f6bc4e4cc19a524111b5",
+    "url": "/davidgkennedy.com/static/js/2.37820ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa8620fedf7fa06ce7d",
+    "url": "/davidgkennedy.com/static/js/main.860cf784.chunk.js"
+  },
+  {
+    "revision": "3e928b349672e5d461e3",
+    "url": "/davidgkennedy.com/static/js/runtime-main.62874f7a.js"
+  }
+]);
