b26161e0a8f3d8cd916c2987ebba813e1b34fdf0
DavidKe91 <davidgkennedy@hotmail.com~>
2023-04-04 20:57:51 +0200

    Updates

---
+++ precache-manifest.ea2711651aa8d0a860468f2ed419ff6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c4daf34c4ef66bc835ebcf8ad55185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0da15e46807f86e5dc8",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "723c316604d97ef80ed3",
+    "url": "/static/css/main.e2ad3e0a.chunk.css"
+  },
+  {
+    "revision": "d0da15e46807f86e5dc8",
+    "url": "/static/js/2.0ca90c7b.chunk.js"
+  },
+  {
+    "revision": "e1e3f1d979a7f864c378dfb23615ac71",
+    "url": "/static/js/2.0ca90c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723c316604d97ef80ed3",
+    "url": "/static/js/main.1d5ab82e.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dk-portfolio/precache-manifest.beaaecee89bf77e35406fae7f61f0aa6.js"
+  "/precache-manifest.ea2711651aa8d0a860468f2ed419ff6d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dk-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
